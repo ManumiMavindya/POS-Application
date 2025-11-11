69ec109e9fca5864638fcf57d1991cca2a4315d3
manummimavindya <manummi2023@gmail.com>
2025-11-11 11:40:22 +0530

    updated CustomerController.js

--- Controller/CustomerController.js
+++ Controller/CustomerController.js
@@ -1,2 +1,58 @@
-import {customerDB} from "../DB/CustomerDB";
-import {CustomerDTO} from "../DTO/CustomerDTO";
+import {customerDB} from "../DB/CustomerDB.js";
+import {CustomerDTO} from "../DTO/CustomerDTO.js";
+
+// ============================== Generate Customer ID ================================================
+
+const generateId = () => {
+    if (customerDB.length === 0){
+        return "C001";
+    }
+    let lastId = customerDB[customerDB.length-1].getId();
+    let number = parseInt(lastId.substring(1));
+    number++;
+    return "C" + number.toString().padStart(3, "0");
+};
+
+// ============================== Load Customer Table Data  ================================================
+
+const loadTable = () => {
+
+    $('#cusTable').empty();
+
+    customerDB.forEach(cus => {
+        let row = `
+            <tr>
+                <td>${cus.getId()}</td>
+                <td>${cus.getName()}</td>
+                <td>${cus.getPhone()}</td>
+                <td>${cus.getEmail()}</td>
+                <td>${cus.getAddress()}</td>
+            </tr>
+        `;
+        $('#cusTable').append(row);
+    });
+
+};
+
+// ============================== Save Customer (button)================================================
+
+let customerId = undefined;
+
+$(document).on("click", "#cusSave", function (){
+
+    let name = $('#inputName').val();
+    let contact = $('#inputContact').val();
+    let email = $('#inputEmail').val();
+    let address = $('#inputAddress').val();
+
+    if (name === '' || contact === '' || email === '' || address === ''){
+        alert('please fill the all values');
+        return;
+    }
+
+    customerDB.push(new CustomerDTO(generateId(), name, contact, email, address));
+    loadTable();
+
+});
+
+
