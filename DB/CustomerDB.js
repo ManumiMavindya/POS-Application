6279e9191c0ccd8c8e3db1bc9a449d3db321a727
manummimavindya <manummi2023@gmail.com>
2025-11-11 11:40:45 +0530

    updated CustomerDB.js

--- DB/CustomerDB.js
+++ DB/CustomerDB.js
@@ -1,11 +1,1 @@
-export const customerDB = [
-
-    // {
-    //     id: "C001",
-    //     name: "Manumi Mavindya",
-    //     phone: "0772796699",
-    //     email: "manumi2023@gmail.com",
-    //     address: "Galle"
-    //
-    // }
-];
+export const customerDB = [];
